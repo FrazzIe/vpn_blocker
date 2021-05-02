6c150b0e4393e4bb711ec6449e5b0605244c09a4
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-05-02 21:03:09 +0100

    add email and flag cvars and create api params

--- main.cpp
+++ main.cpp
@@ -2,7 +2,33 @@
 
 #include <string>
 
-PCL int OnInit(){ // Function executed after the plugin is loaded on the server.
+cvar_t *vpnEmail;
+cvar_t *vpnFlags;
+const std::string apiUrl("http://check.getipintel.net/check.php?ip=");
+std::string apiUrlParams;
+
+PCL int OnInit(){ //Function executed after the plugin is loaded on the server.
+	vpnEmail = (cvar_t*)Plugin_Cvar_RegisterString("vpn_blocker_email", "", 0, "");
+	vpnFlags = (cvar_t*)Plugin_Cvar_RegisterString("vpn_blocker_flag", "m", 0, "");
+
+	if (!vpnEmail->string[0]) {
+		Plugin_PrintError("Init failure. Cvar vpn_blocker_email is not set\n");
+		return -1;
+	}
+
+	if (!vpnFlags->string[0]) {
+		Plugin_PrintError("Init failure. Cvar vpn_blocker_flag is not set\n");
+		return -1;
+	} else if (vpnFlags->string[1] || (vpnFlags->string[0] != 'm' && vpnFlags->string[0] != 'b')) {
+		Plugin_PrintError("Init failure. Cvar vpn_blocker_flag is invalid (use m or b)\n");
+		return -1;
+	}
+
+	std::string vpnEmailStr(vpnEmail->string);
+	std::string vpnFlagStr(vpnFlags->string);
+
+	apiUrlParams = "&contact=" + vpnEmailStr + "&flags=" + vpnFlagStr;
+
 	return 0;
 }
 
