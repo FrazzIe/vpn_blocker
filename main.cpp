1574d27917d7d77a4cbabcabf25bea3d5c68bbc4
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-05-04 19:39:35 +0100

    update ver

--- main.cpp
+++ main.cpp
@@ -325,7 +325,7 @@ PCL void OnInfoRequest(pluginInfo_t *info) { //Function used to obtain informati
 	info->handlerVersion.minor = PLUGIN_HANDLER_VERSION_MINOR;
 
 	info->pluginVersion.major = 0;
-	info->pluginVersion.minor = 1; //Plugin version
+	info->pluginVersion.minor = 2; //Plugin version
 
 	strncpy(info->fullName, "VPN Blocker", sizeof(info->fullName)); //Full plugin name
 	strncpy(info->shortDescription, "Prevent VPN usage by player", sizeof(info->shortDescription)); //Short plugin description
