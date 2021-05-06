24a87e899ea623331c728cad07a492a57bc8a221
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-05-06 05:07:59 +0100

    fix casing

--- ipcache.cpp
+++ ipcache.cpp
@@ -5,14 +5,14 @@
 #include <string>
 #include <chrono>
 
-int64_t getSystemEpoch() {
+int64_t GetSystemEpoch() {
 	return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 }
 
 std::unordered_map<std::string, IPInfo> IPCache::ipMap = {};
 
 void IPCache::Insert(std::string addr, float probability) {
-	ipMap.insert(addr, IPInfo(probability, getSystemEpoch() + cacheLength));
+	ipMap.insert(addr, IPInfo(probability, GetSystemEpoch() + cacheLength));
 }
 
 IPInfo IPCache::Fetch(std::string addr) {
